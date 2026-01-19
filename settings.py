5e1dc4b824f226c23b032c9702b3eae549964832
XuanShine <xuan.ng@hotmail.com>
2026-01-19 01:29:25 +0100

    Secret_key tests

--- settings.py
+++ settings.py
@@ -62,7 +62,7 @@ SMTP_TLS = False
 
 # session settings
 SESSION_TYPE = "cookies"
-SESSION_SECRET_KEY = None  # or replace with your own secret
+SESSION_SECRET_KEY = "test_secret_key_for_development"  # or replace with your own secret
 MEMCACHE_CLIENTS = ["127.0.0.1:11211"]
 
 # Disable default login when using OAuth
